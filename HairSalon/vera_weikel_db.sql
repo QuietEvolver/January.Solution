7e23cbadd74b38f9b5eee878b859f21cb9f79e93
Vera Weikel <vera.weikel@gmail.com>
2020-01-27 05:42:34 -0800

    Update to include bools for appt times and db seeds.

--- HairSalon/vera_weikel_db.sql
+++ HairSalon/vera_weikel_db.sql
@@ -25,20 +25,20 @@ USE vera_weikel_db;
 
 IF EXISTS DROP TABLE clients;  
 CREATE TABLE clients (
-  ClientId int(11) NOT NULL AUTO_INCREMENT,
+  ClientId INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   ClientName varchar(45) DEFAULT NULL,
-  ClientTimeSlotRequested varchar(255) DEFAULT NULL,
-  ClientTimeSlotBooked varchar(255) DEFAULT NULL,
+  ClientTimeSlotRequested BOOLEAN DEFAULT false,
+  ClientTimeSlotBooked BOOLEAN DEFAULT false,
   StylistId int(11) NOT NULL,
   PRIMARY KEY (ClientId)
 )
 IF EXISTS DROP TABLE stylysts;
 CREATE TABLE stylysts (
-  StylistId int(11) NOT NULL AUTO_INCREMENT,
+  StylistId INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   StylistName varchar(45) DEFAULT NULL,
   StylistSpecialty varchar(255) DEFAULT NULL,
-  StylistTimeSlotAvailable varchar(255) DEFAULT NULL,
-  StylistTimeSlotBooked varchar(255) DEFAULT NULL,
+  StylistTimeSlotAvailable BOOLEAN DEFAULT false,
+  StylistTimeSlotBooked BOOLEAN DEFAULT false,
   Address varchar(45) DEFAULT NULL,
   Phone int(45) DEFAULT NULL,
   ClientId int(11) DEFAULT NULL,
@@ -54,3 +54,25 @@ SELECT
      CAST('2007-05-08 12:35:29. 1234567 +12:15' AS time(7)) AS 'Time'   
     ,CAST('2007-05-08 12:35:29. 1234567 +12:15' AS date) AS 'Date' 
 GO
+
+-- Insert a set of records.
+INSERT INTO clients (ClientName) VALUES ('tom');
+INSERT INTO clients (ClientName) VALUES ('chelsea');
+INSERT INTO clients (ClientName) VALUES ('randy');
+INSERT INTO clients (ClientName) VALUES ('pedro');
+INSERT INTO clients (ClientName) VALUES ('barten');
+INSERT INTO clients (ClientName, ClientTimeSlotRequested, StylistId) VALUES ('500', true);
+INSERT INTO clients (ClientName, ClientTimeSlotBooked,StylistId) VALUES ('430', false);
+INSERT INTO clients (ClientName, ClientTimeSlotBooked,StylistId) VALUES ('500', true);
+INSERT INTO clients (ClientName) VALUES ('anita');
+
+
+INSERT INTO stylysts (StylistSpecialty) VALUES ('trim');
+INSERT INTO stylysts (StylistSpecialty) VALUES ('buzz cuts');
+INSERT INTO stylysts (StylistSpecialty) VALUES ('prom');
+INSERT INTO stylysts (StylistSpecialty) VALUES ('pizzaz colors');
+INSERT INTO stylysts (StylistSpecialty) VALUES ('mani');
+INSERT INTO stylysts (StylistSpecialty, StylistTimeSlotRequested, ClientId) VALUES ('500', true,6);
+INSERT INTO stylysts (StylistSpecialty, StylistTimeSlotBooked,ClientId) VALUES ('430', false, 7);
+INSERT INTO stylysts (StylistSpecialty, StylistTimeSlotBooked,ClientId) VALUES ('500', true, 8);
+INSERT INTO stylysts (StylistSpecialty) VALUES ('pediß');
